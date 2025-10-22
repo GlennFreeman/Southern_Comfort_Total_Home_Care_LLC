2ae5ef689cf43333788a085833f2e04998ce6697
Glenn Freeman <glenn.freeman.business@gmail.com>
2025-10-22 13:55:39 -0400

    implemented steps from astro guide

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://glennfreeman.github.io',
+    base: '/Southern_Comfort_Total_Home_Care_LLC'
+});
